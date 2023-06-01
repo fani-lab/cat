c9d4410eda902501d274645a14d5d2f1efc512ba
farinamhz <f.hemmatizadeh@gmail.com>
2023-06-01 17:50:28 -0400

    evaluation added for in LADy's baseline

--- experiments/experiment_test.py
+++ experiments/experiment_test.py
@@ -1,6 +1,12 @@
 """Experiment on the test data."""
 import json
+import os
+
 import numpy as np
+
+# LADy_eval
+import pytrec_eval
+import pandas as pd
 
 from cat.simple import get_scores, attention, rbf_attention
 from cat.dataset import restaurants_test
@@ -16,19 +22,29 @@ BEST_RBF = {"n_noun": 200}
 
 if __name__ == "__main__":
 
-    scores = defaultdict(dict)
-    r = Reach.load("embeddings/restaurant_vecs_w2v.vec",
-                   unk_word="<UNK>")
+    # LADy_eval
+    output_path = "../output/"
+    if not os.path.isdir(output_path):
+        os.makedirs(output_path)
+    metrics = ['P', 'recall', 'ndcg_cut', 'map_cut', 'success']
+    topkstr = '1,5,10,100'
+    metrics_set = set()
+    for m in metrics:
+      metrics_set.add(f'{m}_{topkstr}')
 
-    d = json.load(open("data/nouns_restaurant.json"))
+    scores = defaultdict(dict)
+    r = Reach.load("../embeddings/toy_vecs_w2v.vec",
+                   unk_word="<UNK>")
+    d = json.load(open("../data/toy_nouns.json"))
 
     nouns = Counter()
     for k, v in d.items():
         if k.lower() in r.items:
             nouns[k.lower()] += v
 
-    embedding_paths = ["embeddings/restaurant_vecs_w2v.vec"]
-    bundles = ((rbf_attention, attention), embedding_paths)
+    embedding_paths = ["../embeddings/toy_vecs_w2v.vec"]
+    # bundles = ((rbf_attention, attention), embedding_paths)
+    bundles = ((rbf_attention, ), embedding_paths)
 
     for att, path in product(*bundles):
         r = Reach.load(path, unk_word="<UNK>")
@@ -39,32 +55,59 @@ if __name__ == "__main__":
             candidates, _ = zip(*nouns.most_common(BEST_ATT["n_noun"]))
 
         aspects = [[x] for x in candidates]
-
-        for idx, (instances, y, label_set) in enumerate(restaurants_test()):
-
+        sorted_output = []
+        for idx, (instances, y, label_set, subset_labels, gold) in enumerate(restaurants_test()):
+            # print("label_set", label_set)
             s = get_scores(instances,
                            aspects,
                            r,
-                           label_set,
+                           subset_labels,
                            gamma=GAMMA,
                            remove_oov=False,
                            attention_func=att)
 
-            y_pred = s.argmax(1)
-            f1_score = precision_recall_fscore_support(y, y_pred)
-            f1_macro = precision_recall_fscore_support(y,
-                                                       y_pred,
-                                                       average="weighted")
-            scores[(att, path)][idx] = (f1_score, f1_macro)
+            # print("predicted", s)
+            # print("subset_labels", subset_labels)
+            # print("gold", list(gold))
+            output = [[(label, value) for value, label in zip(sublist, subset_labels)] for sublist in s]
+            sorted_output = [sorted(sublist, key=lambda x: x[1], reverse=True) for sublist in output]
+            # print("output", sorted_output)
 
-    att_score = {k: v for k, v in scores.items() if k[0] == attention}
-    att_per_class = [[z[x][0][:-1] for x in range(3)]
-                     for z in att_score.values()]
-    att_per_class = np.stack(att_per_class).mean(0)
-    att_macro = np.mean([v[2][1][:-1] for v in att_score.values()], 0)
+            qrel = dict()
+            run = dict()
 
-    rbf_score = {k: v for k, v in scores.items() if k[0] == rbf_attention}
-    rbf_per_class = [[z[x][0][:-1] for x in range(3)]
-                     for z in rbf_score.values()]
-    rbf_per_class = np.stack(rbf_per_class).mean(0)
-    rbf_macro = np.mean([v[2][1][:-1] for v in rbf_score.values()], 0)
+            for i, word in enumerate(gold):
+                q_key = 'q{}'.format(i)
+                qrel[q_key] = {word: 1}
+
+            for i, sublist in enumerate(sorted_output):
+                q_key = 'q{}'.format(i)
+                run[q_key] = {}
+                for j, (word, _) in enumerate(sublist):
+                    run[q_key][word] = len(sublist) - j
+
+            print("qrel: ", qrel)
+            print("run: ", run)
+
+            print(f'pytrec_eval for {metrics_set} ...')
+            df = pd.DataFrame.from_dict(pytrec_eval.RelevanceEvaluator(qrel, metrics_set).evaluate(run))
+            df_mean = df.mean(axis=1).to_frame('mean')
+            df_mean.to_csv(f'{output_path}pred.eval.mean.csv')
+            # y_pred = s.argmax(1)
+    #         f1_score = precision_recall_fscore_support(y, y_pred)
+    #         f1_macro = precision_recall_fscore_support(y,
+    #                                                    y_pred,
+    #                                                    average="weighted")
+    #         scores[(att, path)][idx] = (f1_score, f1_macro)
+    #
+    # att_score = {k: v for k, v in scores.items() if k[0] == attention}
+    # att_per_class = [[z[x][0][:-1] for x in range(3)]
+    #                  for z in att_score.values()]
+    # att_per_class = np.stack(att_per_class).mean(0)
+    # att_macro = np.mean([v[2][1][:-1] for v in att_score.values()], 0)
+    #
+    # rbf_score = {k: v for k, v in scores.items() if k[0] == rbf_attention}
+    # rbf_per_class = [[z[x][0][:-1] for x in range(3)]
+    #                  for z in rbf_score.values()]
+    # rbf_per_class = np.stack(rbf_per_class).mean(0)
+    # rbf_macro = np.mean([v[2][1][:-1] for v in rbf_score.values()], 0)
